ef53db6593ad378ef491205eba7acec0c7fa9e85
DavidLindström <davlin@creatorise.com>
2020-12-12 06:12:18 +0100

    Entry uploads to database on change

---
+++ public/src/entries.php
@@ -0,0 +1,32 @@
+<?php
+const path = '../../data/entries.json';
+
+if ($_GET['update']){
+	$newEntry = [
+		'food' => $_POST['food'],
+		'datetime' => $_POST['datetime'],
+		'id' => $_POST['id'],
+	];
+	var_dump($newEntry);
+
+	$entries_prior = json_decode(file_get_contents(path));
+	var_dump($entries_prior);
+	$entries_prior_hasId = false;
+	$updated_entries = [];
+	foreach ($entries_prior as $key => $entry_prior) {
+		if ($entry_prior->id === $newEntry['id']) {
+			$entries_prior_hasId = true;
+			$updated_entries[] = $newEntry;
+		} else {
+			$updated_entries[] = $entry_prior;
+		}
+	}
+	if (!$entries_prior_hasId) {
+		$updated_entries[] = $newEntry;
+	}
+
+	usort($updated_entries, function ($first, $second) {
+		return $first->date > $second->date;
+	});
+	file_put_contents(path, json_encode($updated_entries));
+}
--- public/static/entries.js
+++ public/static/entries.js
@@ -1,35 +1,48 @@
+import { v4 } from '../../node_modules/uuid/dist/esm-browser/index.js';
+
 /* --------------------------------- OnLoad --------------------------------- */
+
+// HTML node in which all entries are put
+const m_entries = document.querySelector('.m_entries');
+
+// HTML node of current date
+const a_date = document.querySelector('.a_date');
+a_date.value = formatDate(new Date());
+
 
 // State object containing semantics of an empty entry
 let entryTemplate;
+
 // Setting the entry template to semantics recieved from server
 entryTemplate_set().then(() => {
-	m_entries.appendChild(new_entry_withCurrentTime(entryTemplate));
+	m_entries.appendChild(new_entry_withCurrentTime(v4(), entryTemplate));
 });
-
-// HTML node in which all entries are put
-const m_entries = document.querySelector(
-	'.m_entries'
-);
-
 
 /* ------------------------------- Procedures ------------------------------- */
 
 async function entryTemplate_set() {
-	entryTemplate = await getNodeFromFetchedView('entry');
+	// Fetching entry template view from server
+	let req = await fetch(
+		'https://creatorise.com/avalli/public/src/views.php?getView=entry'
+		);
+	let entryHtml = await req.text();
+
+	entryTemplate = createNodeFromString(entryHtml);
 }
 
-function new_entry_withCurrentTime(entryTemplate) {
-	let newEntry = new_entry(entryTemplate);
+function new_entry_withCurrentTime(id, entryTemplate) {
+	let newEntry = new_entry(id, entryTemplate);
 	newEntry.time.value = formatTime(new Date());
 	return newEntry;
 }
+
+
 
 /* --------------------------------- Events --------------------------------- */
 
 function a_entry_enterKeyup(e, entryTemplate) {
 	if (e.keyCode === 13) {
-		const newEntry = new_entry(entryTemplate);
+		const newEntry = new_entry(v4(), entryTemplate);
 		
 		e.target.parentElement.after(newEntry);
 		newEntry.firstElementChild.select();
@@ -46,40 +59,36 @@ function a_entry_enterKeyup(e, entryTemplate) {
 	}
 }
 
+function a_entry_onchange(e, id) {
+	let a_entry_data = new FormData(e.target.parentElement);
+	let datetime = Date.parse(
+		a_date.value + 'T' + a_entry_data.get('time')
+	);
+	a_entry_data.append('datetime', datetime);
+	a_entry_data.delete('time');
+	a_entry_data.append('id', id);
+	
+	fetch('https://creatorise.com/avalli/public/src/entries.php?update=true', 
+		{
+			method: 'POST',
+			body: a_entry_data
+		})
+		.then((req) => req.text())
+		.then((res) => console.log('res:', res)
+	);
+}
+
 /* ----------------------------- Pure functions ----------------------------- */
 
-// Fetching selected view from templates on server
-async function getNodeFromFetchedView(templateName) {
-	let req = await fetch(
-		'https://creatorise.com/avalli/public/src/templating.php?getView='
-		 + templateName
-	);
-	let innerHtml = await req.text();
-
-	// Set node from text respons
-	const temp = document.createElement('template');
-	innerHtml = innerHtml.trim();
-	temp.innerHTML = innerHtml;
-	return temp.content.firstChild;
-}
-
-function new_entry_withSetTime(entryTemplate, formattedTime) {
-	let newEntry = new_entry(entryTemplate);
-	newEntry.time.value = formattedTime;
-	return newEntry;
-}
-
-function new_entry(entryTemplate) {
+function new_entry(id, entryTemplate) {
 	let newEntry = entryTemplate.cloneNode(true);
-	newEntry = addingEntryEventListeners(newEntry, entryTemplate);
+	newEntry = addingEntryEventListeners(newEntry, id, entryTemplate);
 	return newEntry;
 }
 
-function addingEntryEventListeners(
-	newEntry,
-	entryTemplate
-) {
+function addingEntryEventListeners(newEntry, id, entryTemplate) {
 	newEntry.addEventListener('submit', preventDefault);
+	newEntry.addEventListener('change', (e) => a_entry_onchange(e, id));
 	for (let child of newEntry.children) {
 		child.addEventListener(
 			'keyup',
@@ -93,15 +102,18 @@ function preventDefault(e) {
 	e.preventDefault();
 }
 
+function createNodeFromString(html) {
+	const temp = document.createElement('template');
+	html = html.trim();
+	temp.innerHTML = html;
+	return temp.content.firstChild;
+}
+
 /* Date formatting functions */
 
 function formatTime(date) {
-	let hour =
-		(date.getHours() < 10 ? '0' : '') +
-		date.getHours();
-	let minute =
-		(date.getMinutes() < 10 ? '0' : '') +
-		date.getMinutes();
+	let hour = (date.getHours() < 10 ? '0' : '') + date.getHours();
+	let minute = (date.getMinutes() < 10 ? '0' : '') + date.getMinutes();
 	//let second = (date.getSeconds() < 10 ? "0" : "") + date.getSeconds();
 
 	return hour + ':' + minute; // + ":" + second;
@@ -111,10 +123,8 @@ function formatDate(date) {
 	return (
 		date.getFullYear() +
 		'-' +
-		(date.getMonth() + 1 < 10 ? '0' : '') +
-		(date.getMonth() + 1) +
+		(date.getMonth() + 1 < 10 ? '0' : '') + (date.getMonth() + 1) +
 		'-' +
-		(date.getDate() < 10 ? '0' : '') +
-		date.getDate()
+		(date.getDate() < 10 ? '0' : '') + date.getDate()
 	);
 }
